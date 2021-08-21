d32eeb605dfddff309e73d36f62b9e312d7c5027
JUNG EUNJIN <65335952+jayjinnie@users.noreply.github.com>
2021-08-21 15:18:51 +0900

    Create requests.py

---
+++ 3_requests/requests.py
@@ -0,0 +1,15 @@
+import requests
+
+res = requests.get("http://google.com")
+print("응답코드 :", res.status_code) # 200이상이면 정상
+
+if res.status_code == requests.codes.ok:
+     print("정상입니다.")
+else:
+    print("문제 발생. [에러코드 ", res.status_code, "]")
+
+res.raise_for_status()
+print("웹 스크래핑을 진행합니다")
+
+with open("mygoogle.html", "w", encoding="utf8") as f:
+    f.write(res.text)
